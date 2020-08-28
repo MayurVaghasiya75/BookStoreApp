4195bced37635ba637104e5ddaa9bf048aa79f92
Mayur Vaghasiya <mayurvaghasiya75@gmail.com>
2020-08-28 16:48:54 +0930

    commit bookstore

--- BookStoreApp/Program.cs
+++ BookStoreApp/Program.cs
@@ -21,6 +21,7 @@ namespace BookStoreApp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
                 });
     }
 }
